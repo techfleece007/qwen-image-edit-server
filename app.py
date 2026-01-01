c3a795c4c196d1d7a9fc3dc0359f9830b3a2aaf8
techfleece007 <techfleece007@gmail.com>
2026-01-01 04:15:50 +0400

    app.py to fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from io import BytesIO
 import base64
 from typing import Optional
 
-# Workaround for torch.xpu AttributeError in diffusers
+# Workaround for torch.xpu AttributeError in diffuserss
 # Some versions of diffusers try to access torch.xpu which doesn't exist in standard PyTorch
 if not hasattr(torch, 'xpu'):
     class XPUModule:
